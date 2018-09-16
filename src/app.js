4cb764078db1d75540c95b11fc8fb34b92be651d
othman-shamla <othmansh2010@gmail.com>
2018-09-16 16:01:13 +0300

    insatlling and configure multer, cookie-parser, request

--- src/app.js
+++ src/app.js
@@ -3,17 +3,23 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const compression = require('compression');
-const controllers = require('./controllers/index');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
 
+const controllers = require('./controllers/index');
+const helpers = require('./views/helpers/index');
+
+const upload = multer();
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(compression());
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'hbs');
+app.use(cookieParser());
 app.engine(
   'hbs',
   exphbs({
@@ -21,10 +27,10 @@ app.engine(
     layoutsDir: path.join(__dirname, 'views', 'layouts'),
     partialsDir: path.join(__dirname, 'views', 'partials'),
     defaultLayout: 'main',
+    helpers,
   }),
-
 );
 
+app.use(upload.single('imageURL'));
 app.use(controllers);
-
 module.exports = app;
