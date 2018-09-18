b4cfb76c5cc4cefc35fbcbfb0574296369e96014
MarwanElkhoudary <marwangaza@hotmail.com>
2018-09-18 10:52:00 +0300

    add validation and style related to #13

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,41 +1,40 @@
-const fName = document.getElementById("fname");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const adduser = document.getElementById("signup");
-const fnamerr = document.getElementById("fnamerr");
-const lnamerr = document.getElementById("lnamerr");
-const emailerr = document.getElementById("emailerr");
-const passerr = document.getElementById("passerr");
+const fName = document.getElementById('fname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const adduser = document.getElementById('signup');
+const fnamerr = document.getElementById('fnamerr');
+const lnamerr = document.getElementById('lnamerr');
+const emailerr = document.getElementById('emailerr');
+const passerr = document.getElementById('passerr');
 
 
 const displayErr = (errElem, errMsg) => {
   errElem.textContent = errMsg;
 };
 const checkFNam = () => {
-  if (fName.value === "") {
-    displayErr(fnamerr, " name is required");
+  if (fName.value === '') {
+    displayErr(fnamerr, ' name is required');
   }
 };
 
 const checkEmail = () => {
   if (email.validity.typeMismatch) {
-    displayErr(emailerr, "NOT vaild email");
+    displayErr(emailerr, 'NOT vaild email');
   } else if (email.validity.valueMissing) {
-    displayErr(emailerr, "Email is required");
+    displayErr(emailerr, 'Email is required');
   } else {
-    displayErr(emailerr, "");
+    displayErr(emailerr, '');
     return true;
   }
 };
 
 
-fName.addEventListener("focusout", checkFNam);
-email.addEventListener("focusout", checkEmail);
+fName.addEventListener('focusout', checkFNam);
+email.addEventListener('focusout', checkEmail);
 
 
-adduser.addEventListener("submit", e => {
-  if (!checkFNam() || !checkEmail() || !checkConfirmPw()) {
-
+adduser.addEventListener('submit', (e) => {
+  if (!checkFNam() || !checkEmail()) {
     e.preventDefault();
   }
 });
