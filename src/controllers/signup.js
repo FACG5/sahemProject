ed07406bf7c78e7a33af1880726c88f4d71885f1
MarwanElkhoudary <marwangaza@hotmail.com>
2018-09-22 11:16:05 +0300

    add upload feature to signuo related to #13

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,6 +1,8 @@
 const addUser = require('../database/queries/addUser');
 const hashpassword = require('../utillity/hashpassword');
 const { createCookie } = require('../utillity/authentication');
+const imgur = require('./../utillity/uploadPhoto');
+
 
 exports.get = (req, res) => {
   res.render('signup', { js: 'js/signup.js', css: 'css/signup.css' });
@@ -10,45 +12,50 @@ exports.post = (req, response) => {
   const data = req.body;
 
   const {
-    name, email, pass, location, spec, occupation, linkedin, facebook, mobile, img,
+    name, email, pass, location, spec, occupation, linkedin, facebook, mobile,
     description,
   } = data;
+  imgur.upload(req.file.buffer)
+    .then((result) => {
+      const img = result.data.link;
 
-  if (data) {
-    hashpassword(pass, (error, hash) => {
-      if (error) {
-        response.render('signup', { msg: error });
-      } else {
-        const obj = {
-          name,
-          email,
-          hash,
-          location,
-          spec,
-          occupation,
-          linkedin,
-          facebook,
-          mobile,
-          img,
-          description,
-        };
-        addUser(obj, (errr, user) => {
-          if (errr) {
+
+      if (data) {
+        hashpassword(pass, (error, hash) => {
+          if (error) {
             response.render('signup', { msg: error });
           } else {
-            createCookie({ id: user.id, name: user.name }, (err, token) => {
-              if (err) response.render('/login', { css: 'css/login.css' });
-              else {
-                response.setHeader(
-                  'Set-Cookie',
-                  `data=${token};httpOnly;Max-age=99999999999`,
-                );
-                response.redirect('/');
+            const obj = {
+              name,
+              email,
+              hash,
+              location,
+              spec,
+              occupation,
+              linkedin,
+              facebook,
+              mobile,
+              img,
+              description,
+            };
+            addUser(obj, (errr, user) => {
+              if (errr) {
+                response.render('signup', { msg: error });
+              } else {
+                createCookie({ id: user.id, name: user.name }, (err, token) => {
+                  if (err) response.render('/login', { css: 'css/login.css' });
+                  else {
+                    response.setHeader(
+                      'Set-Cookie',
+                      `data=${token};httpOnly;Max-age=99999999999`,
+                    );
+                    response.redirect('/');
+                  }
+                });
               }
             });
           }
         });
       }
     });
-  }
 };
