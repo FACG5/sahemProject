810bef300e9185184598f0acf393dc8b0942e745
MarwanElkhoudary <marwangaza@hotmail.com>
2018-09-22 11:29:05 +0300

    modify proposal detalils in userimg related to #45

--- src/database/queries/getProposal.js
+++ src/database/queries/getProposal.js
@@ -1,7 +1,7 @@
 const dbconnection = require('../db_connection');
 
 const getProposal = id => dbconnection.query({
-  text: 'select projects.id, projects.user_id ,projects.camp_end, users.img , users.name as username , projects.abstract, projects.img , projects.name as projectName, (select coalesce(sum(donation.amount), 0) from donation where donation.project_id = $1), projects.fund from projects join users ON users.id = projects.user_id where projects.id = $1',
+  text: 'select projects.id, projects.user_id ,projects.camp_end, users.img as userimg , users.name as username , projects.abstract, projects.img , projects.name as projectName, (select coalesce(sum(donation.amount), 0) from donation where donation.project_id = $1), projects.fund from projects join users ON users.id = projects.user_id where projects.id = $1',
   values: [id],
 });
 
